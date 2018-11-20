b93d4aac0c8d9aaa72654b63ef2624e2ea109563
MAVEAU JÉRÉMY <jeremy.maveau@viacesi.fr>
2018-11-20 13:57:22 +0100

    Done : - Get process list - Get details of a process
    Todo :
    - Listen on windows' processes

---
+++ Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace process_list
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.t_list_process = new System.Windows.Forms.TextBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.list_process = new System.Windows.Forms.ListBox();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.dgv_process = new System.Windows.Forms.DataGridView();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_process)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // t_list_process
+            // 
+            this.t_list_process.Location = new System.Drawing.Point(0, 0);
+            this.t_list_process.Multiline = true;
+            this.t_list_process.Name = "t_list_process";
+            this.t_list_process.Size = new System.Drawing.Size(631, 404);
+            this.t_list_process.TabIndex = 0;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(635, 430);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.dgv_process);
+            this.tabPage1.Controls.Add(this.list_process);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(627, 404);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.t_list_process);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(627, 404);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // list_process
+            // 
+            this.list_process.FormattingEnabled = true;
+            this.list_process.Location = new System.Drawing.Point(0, 0);
+            this.list_process.Name = "list_process";
+            this.list_process.Size = new System.Drawing.Size(220, 407);
+            this.list_process.TabIndex = 0;
+            this.list_process.Click += new System.EventHandler(this.select_process);
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(627, 404);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "tabPage3";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "n_notifyIcon_Test";
+            this.notifyIcon1.Visible = true;
+            // 
+            // dgv_process
+            // 
+            this.dgv_process.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dgv_process.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dgv_process.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_process.Location = new System.Drawing.Point(226, 6);
+            this.dgv_process.Name = "dgv_process";
+            this.dgv_process.RowHeadersVisible = false;
+            this.dgv_process.Size = new System.Drawing.Size(395, 392);
+            this.dgv_process.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(650, 490);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_process)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox t_list_process;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.ListBox list_process;
+        private System.Windows.Forms.DataGridView dgv_process;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace process_list
+{
+    public partial class Form1 : Form
+    {
+        private Process[] T_process;
+        public Form1()
+        {
+            InitializeComponent();
+            dgv_process.Columns.Add("Prop", "Properties");
+            dgv_process.Columns.Add("Val", "Values");
+            dgv_process.Hide();
+            get_process();
+        }
+
+        private void get_process(){
+            List<Process> l_process = new List<Process>();
+            Process[] p_list = Process.GetProcesses();
+            foreach (var item in p_list)
+            {
+                list_process.Items.Add(item.ProcessName);
+                l_process.Add(item);
+            }
+            T_process = l_process.ToArray();
+            foreach (var prop in T_process[0].GetType().GetProperties())
+            {
+                dgv_process.Rows.Add(prop.Name, "");
+            }
+            dgv_process.Show();
+        }
+
+        private void select_process(object sender, EventArgs e)
+        {
+            Process cur_proc = T_process[list_process.SelectedIndex];
+            int i = 0;
+            foreach (var prop in cur_proc.GetType().GetProperties())
+            {
+                dgv_process.Rows[i].Cells[0].Value = prop.Name;
+                try
+                {
+                    dgv_process.Rows[i].Cells[1].Value = prop.GetValue(cur_proc);
+                }
+                catch (Exception)
+                {
+                    dgv_process.Rows[i].Cells[1].Value = "Unavailable";
+                }
+                i++;
+            }
+        }
+
+    }
+}
